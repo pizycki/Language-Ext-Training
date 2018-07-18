ab43bf12f0c08cad71cc211e687163e587c0107a
Izycki <Pawel.Izycki@BritishCouncil.Org>
2018-07-18 12:33:37 +0200

    WorkingWithMultipleMonadicTypes

---
+++ LanguageExt-Training/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using FluentAssertions;
+using LanguageExt;
+using Xunit;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt_Training
+{
+    public class WorkingWithMultipleMonadicTypes
+    {
+        Guid PaymentId = Guid.Parse("ab8084ca-ae51-4f59-a766-63a02309d016");
+        Result<Guid> StartPayment(string appCode, string apiKey) => new Result<Guid>(PaymentId);
+
+        [Fact]
+        public void linq_way_and_matches()
+        {
+            Option<string> appCode = Some("App Code");
+            Option<string> apiKey = Some("API Key");
+
+            Option<Result<Guid>> result = from ac in appCode
+                                          from ak in apiKey
+                                          select StartPayment(ac, ak);
+
+            var paymentId = result.Match(
+                None: () => "Bad request.",
+                Some: res => res.Match(
+                    Succ: id => id.ToString(),
+                    Fail: ex => ex.Message));
+
+            paymentId.Should()
+                .BeOfType<string>()
+                .And
+                .BeEquivalentTo(PaymentId.ToString());
+
+            /* Drawbacks:
+             * We don't know what went wrong (was it app code or api key missing)
+             * We have only two level monadic type (which is good for us).
+             */
+        }
+
+        [Fact]
+        public void linqWay()
+        {
+            Option<string> appCode = Some("App Code");
+            Option<string> apiKey = Some("API Key");
+
+            app
+
+        }
+
+
+    }
+}
---
+++ LanguageExt-Training/ReducingMatchCount.cs
@@ -0,0 +1,19 @@
+using System;
+using LanguageExt;
+using Xunit;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt_Training
+{
+    public class ReducingMatchCount
+    {
+        [Fact]
+        public void ElTesto()
+        {   
+            Option<string> appCode = Some("App Code");
+            Option<string> apiKey = Some("API Key");
+
+            
+        }
+    }
+}
